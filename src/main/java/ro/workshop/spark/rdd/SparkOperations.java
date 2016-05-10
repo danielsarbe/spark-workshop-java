3433a25806bfb7683d8d84d0f7582371ecfd2c17
Daniel Sarbe <dsarbe@sdl.com>
2016-05-10 19:17:43 +0300

    Small changes to Spark Operations

--- src/main/java/ro/workshop/spark/rdd/SparkOperations.java
+++ src/main/java/ro/workshop/spark/rdd/SparkOperations.java
@@ -43,7 +43,7 @@ public class SparkOperations {
         /**
          For development/testing, select only 10k lines from each file
          Mac/Linux: head -n 10000 f1.json > s_f1.json
-         Windows(using Get-Content command): gc -TotalCount 100000 f1.json > s_f1.json
+         Windows(using Get-Content command): gc -TotalCount 10000 f1.json > s_f1.json
          */
 
         String inputPathBusiness = "data/s_business.json";
